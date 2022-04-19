eba56ed0792e90ec44a11e49eb01f210dce10049
vstebletsov89 <vstebletsov89@github.com>
2022-04-19 22:06:06 +0200

    small fixes

--- internal/app/model/order.go
+++ internal/app/model/order.go
@@ -7,6 +7,6 @@ type Order struct {
 	Login      string    `json:"-"`
 	Number     string    `json:"number"`
 	Status     string    `json:"status"`
-	Accrual    int       `json:"accrual,omitempty"`
+	Accrual    float64   `json:"accrual,omitempty"`
 	UploadedAt time.Time `json:"uploaded_at"`
 }
